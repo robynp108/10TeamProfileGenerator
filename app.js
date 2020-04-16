28380c33f488aebb648753adf95bb24d7ca25e55
robynp108 <robynp108@gmail.com>
2020-04-15 19:35:50 -0500

    code cleanup and add comments

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-
-
+// Initial questions to build employees with classes
 async function askQuestions() {
     try {
         const employee = await inquirer.prompt([
@@ -45,7 +39,6 @@ async function askQuestions() {
                 ]
             },
         ]);
-        // console.log(employee)
         if (employee.role == "manager") {
             const managerInfo = await inquirer.prompt([
                 {
@@ -76,17 +69,13 @@ async function askQuestions() {
             ]);
             return new Intern(employee.name, employee.id, employee.email, internInfo.school);
         };
-        // console.log("inside app.js " + JSON.stringify(employee));
-
-
-
     } catch (err) {
         console.log(err);
     }
 }
 
 
-
+//Begin looping functions for adding multiple employees
 askQuestions().then(
     async (employee) => {
         const employeeObjects = [employee];
@@ -120,9 +109,8 @@ askQuestions().then(
         } catch (err) {
             console.log(err);
         }
-
+        // Generate file/html page of completed employee team
         const result = render(employeeObjects);
-        console.log(result);
 
         fs.writeFile(outputPath, result, function (err) {
 
@@ -133,65 +121,3 @@ askQuestions().then(
         });
     }
 )
-
-// askQuestions().then(
-//     async (employee) => {
-//         const employeeObjects = [employee];
-//         try {
-//             const nextEmployee = await inquirer.prompt([
-//                 {
-//                     type: "list",
-//                     message: "Would you like to add another employee?",
-//                     name: "another",
-//                     choices: [
-//                         "yes",
-//                         "no"
-//                     ]
-//                 },
-//             ]);
-//             if (nextEmployee.another == "yes") {
-//                 employee = await askQuestions();
-//                 employeeObjects.push(employee);
-//             }
-//         } catch (err) {
-//             console.log(err);
-//         }
-
-//         const result = render(employeeObjects);
-//         console.log(result);
-
-//         fs.writeFile(outputPath, result, function (err) {
-
-//             if (err) {
-//                 return console.log(err);
-//             }
-
-//         });
-//     }
-// )
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
